2f2c93fd7f29a6295959cc5b510b050b91df25e3
Lasse Madsen <lasse.madsen93@gmail.com>
2021-10-08 14:38:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
     include_package_data=True,
     platforms='any',
     install_requires=[
+        "visbrain @ git+https://github.com/lassemadsen/visbrain/tarball/master#egg=visbrain
         "numpy>=1.13",
         "pandas",
         "brainstat>=0.2.7",
@@ -50,7 +51,6 @@ setup(
         "matplotlib>=1.5.5",
         "pillow"
     ],
-    dependency_links=["https://github.com/lassemadsen/visbrain/tarball/master#egg=visbrain"],
     classifiers=["Development Status :: 3 - Alpha",
                  'Intended Audience :: Science/Research',
                  'Intended Audience :: Education',
