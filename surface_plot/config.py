03a3702714b9d4344de1d7154813a494c9d98cb4
Lasse Stensvig Madsen <lasse.madsen93@gmail.com>
2021-10-15 12:14:42 +0200

    Add n_vertex to config

--- surface_plot/config.py
+++ surface_plot/config.py
@@ -2,4 +2,7 @@ import pathlib
 # Define surface to use
 SURFACE = {'left': f'{pathlib.Path(__file__).parent.resolve()}/surface_data/mni_icbm152_t1_tal_nlin_sym_09c_left_smooth.gii',
            'right': f'{pathlib.Path(__file__).parent.resolve()}/surface_data/mni_icbm152_t1_tal_nlin_sym_09c_right_smooth.gii',
-           'both': f'{pathlib.Path(__file__).parent.resolve()}/surface_data/mni_icbm152_t1_tal_nlin_sym_09c_both_smooth.gii',}
+           'both': f'{pathlib.Path(__file__).parent.resolve()}/surface_data/mni_icbm152_t1_tal_nlin_sym_09c_both_smooth.gii'}
+
+N_VERTEX = {'left': 81349,
+            'right': 81233}
