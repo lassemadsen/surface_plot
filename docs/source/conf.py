8b48983e24682644ab1af967539742816f8f90c9
Lasse Stensvig Madsen <lasse.madsen93@gmail.com>
2024-11-27 14:25:07 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 import matplotlib
-matplotlib.use('TkAgg')
+matplotlib.use('Qt5Agg')
 
 
 # -- Project information -----------------------------------------------------
