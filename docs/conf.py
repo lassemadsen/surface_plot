af3ea31ff2e9a24ed9693329a57b201bf1247631
Lasse Stensvig Madsen <lasse.madsen93@gmail.com>
2024-11-27 11:40:24 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -45,16 +45,3 @@ sphinx_gallery_conf = {
      'examples_dirs': '../../examples',   # path to your example scripts
      'gallery_dirs': 'auto_examples',  # path to where to save gallery generated output
 }
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
